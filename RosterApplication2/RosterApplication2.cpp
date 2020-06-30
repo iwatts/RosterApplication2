8aeca71256b858a38804aae6ff939afdc89d466a
Isaac <isaac.r.watts@gmail.com>
2020-06-30 12:18:15 -0600

    formatting

--- RosterApplication2/RosterApplication2.cpp
+++ RosterApplication2/RosterApplication2.cpp
@@ -6,26 +6,26 @@ using namespace std;
 
 int main()
 {
-    const string studentData[] = {
-        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"
-    };
+	const string studentData[] = {
+		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+		"A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"
+	};
 
-    string userInput = "";
-    cout << "Menu" << endl;
-    cout << "q: Quit" << endl;
+	string userInput = "";
+	cout << "Menu" << endl;
+	cout << "q: Quit" << endl;
 
-    while (userInput != "q") {
-        cin >> userInput;
-        for (int i = 0; i < userInput.length(); i++) {
-            if (isupper(userInput.at(i))) userInput.at(i) = tolower(userInput.at(i));
-        }
-    }
+	while (userInput != "q") {
+		cin >> userInput;
+		for (int i = 0; i < userInput.length(); i++) {
+			if (isupper(userInput.at(i))) userInput.at(i) = tolower(userInput.at(i));
+		}
+	}
 
-    cout << userInput;
+	cout << userInput;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
