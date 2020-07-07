cdd85e21e91a0f28327cb9cf7cb29d0f016dc6ba
Isaac <isaac.r.watts@gmail.com>
2020-07-06 21:10:21 -0600

    better orginization

--- RosterApplication2/RosterApplication2.cpp
+++ RosterApplication2/RosterApplication2.cpp
@@ -11,6 +11,14 @@ int main()
 	string userInput = "";
 	bool recognizedInput;
 	roster classRoster;
+	int i;
+	const string studentData[] = {
+		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+		"A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"
+	};
 
 
 	cout << "Commands" << endl;
@@ -44,11 +52,3 @@ int main()
 	}
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control  
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- RosterApplication2/roster.cpp
+++ RosterApplication2/roster.cpp
@@ -31,18 +31,4 @@ void roster::printInvalidEmails() {
 	return;
 }
 
-roster::roster() {
-	int i;
-	const string studentData[] = {
-		"A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
-		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
-		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
-		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,[firstname],[lastname],[emailaddress],[age], [numberofdaystocomplete3courses],SOFTWARE"
-	};
-
-	for (i = 0; i < 4; i++) {
-		studentData[i];
-		//this->add(parsedStudent);
-	}
-}
+roster::roster() {}
